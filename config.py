cd9900aa30bd9f5390c70a60a96f7da4409a7614
Maciej Balawejder <maciekstar6@gmail.com>
2022-08-16 20:09:07 +0200

    add configs

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-class Config:
+class Base:
     img_size : int = 144
     layers : int = 12
     d_size : int = 768
@@ -8,3 +8,47 @@ class Config:
     p_att : float = 0.1
     p_emb : float = 0.1
     p_mlp : float = 0.1
+
+class L_16(Base):
+    layers : int = 12
+    d_size : int = 1024
+    mlp_size : int = 4096
+    n_heads : int = 16
+    patch_size : int = 16
+    p_att : float = 0.1
+    p_emb : float = 0.1
+    p_mlp : float = 0.1
+
+class L_32(Base):
+    layers : int = 12
+    d_size : int = 1024
+    mlp_size : int = 4096
+    n_heads : int = 16
+    patch_size : int = 32
+    p_att : float = 0.1
+    p_emb : float = 0.1
+    p_mlp : float = 0.1
+
+class B_16(Base):
+    img_size : int = 144
+    layers : int = 12
+    d_size : int = 768
+    mlp_size : int = 3072
+    n_heads : int = 12
+    patch_size : int = 16
+    p_att : float = 0.1
+    p_emb : float = 0.1
+    p_mlp : float = 0.1
+
+class B_32(Base):
+    img_size : int = 144
+    layers : int = 12
+    d_size : int = 768
+    mlp_size : int = 3072
+    n_heads : int = 12
+    patch_size : int = 32
+    p_att : float = 0.1
+    p_emb : float = 0.1
+    p_mlp : float = 0.1
+
+
